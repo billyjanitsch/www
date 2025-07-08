6526eeccb89da11d4da620faa16a5bb9c012828c
Billy Janitsch <billyjanitsch@gmail.com>
2025-07-08 11:43:37 -0400

    Increase image quality

--- app/page.tsx
+++ app/page.tsx
@@ -27,6 +27,7 @@ export default function HomePage() {
         className="max-w-screen max-h-screen object-contain"
         src={cider}
         alt="Billy Janitsch"
+        quality={95}
       />
     </main>
   );
