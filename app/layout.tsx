c23ea0734f48cedc8f94bd067db5a8d3b9a5f17f
Billy Janitsch <billyjanitsch@gmail.com>
2025-07-08 11:52:59 -0400

    Tweak metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,22 @@
+import { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Billy Janitsch",
+  description: "Billy Janitsch on the world wide web",
+  openGraph: {
+    title: "Billy Janitsch",
+    description: "Billy Janitsch on the world wide web",
+    siteName: "Billy Janitsch",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Billy Janitsch",
+    description: "Billy Janitsch on the world wide web",
+  },
+};
 
 interface RootLayoutProps {
   children?: React.ReactNode;
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,5 @@
-import { Metadata } from "next";
 import Image from "next/image";
 import cider from "./cider.png";
-
-export const metadata: Metadata = {
-  title: "Billy Janitsch",
-  description: "Billy Janitsch on the world wide web",
-  openGraph: {
-    title: "Billy Janitsch",
-    description: "Billy Janitsch on the world wide web",
-    siteName: "Billy Janitsch",
-    url: "https://www.billyjanitsch.com",
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Billy Janitsch",
-    description: "Billy Janitsch on the world wide web",
-  },
-};
 
 export default function HomePage() {
   return (
